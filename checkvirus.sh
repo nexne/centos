cf1e12cfedb2a170ff4b20d550d453f348138c71
nexne <nexne@unpak.ac.id>
2018-10-24 17:01:11 +0800

    Delete checkvirus.sh

--- checkvirus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Script untuk memeriksa rootkit pada server menggunakan chkrootkit
-#By Khairil Gunawan
-
-echo "Memulai scanning file - file penting pada server"
-/root/chkrootkit/chkrootkit
-echo "Scanning file selesai"
