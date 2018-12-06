270850b1edb3e58192c9ba0bd659b58dd87d69ab
nexne <nexne@unpak.ac.id>
2018-12-06 21:27:00 +0800

    Update coba.sh

--- coba.sh
+++ coba.sh
@@ -227,7 +227,41 @@ echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 
 # install squid3
 apt-get -y install squid3
-wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/nexne/32n64/master/squid3.conf"
+cat > /etc/squid3/squid.conf <<-END
+acl localhost src 127.0.0.1/32 ::1
+acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
+acl SSL_ports port 443
+acl Safe_ports port 80
+acl Safe_ports port 21
+acl Safe_ports port 443
+acl Safe_ports port 70
+acl Safe_ports port 210
+acl Safe_ports port 1025-65535
+acl Safe_ports port 280
+acl Safe_ports port 488
+acl Safe_ports port 591
+acl Safe_ports port 777
+acl CONNECT method CONNECT
+acl SSH dst xxxxxxxxx-xxxxxxxxx/32
+http_access allow SSH
+http_access allow manager localhost
+http_access deny manager
+http_access allow localhost
+http_access deny all
+http_port 8080
+http_port 3130
+http_port 3000
+http_port 1080
+http_port 8000
+http_port 8888
+http_port 3128
+coredump_dir /var/spool/squid3
+refresh_pattern ^ftp: 1440 20% 10080
+refresh_pattern ^gopher: 1440 0% 1440
+refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
+refresh_pattern . 0 20% 4320
+visible_hostname boostvpn
+END
 sed -i $MYIP2 /etc/squid3/squid.conf;
 service squid3 restart
 
