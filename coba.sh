d2d76e8f012b558049deaf2f1261f91a22bdb0a4
nexne <nexne@unpak.ac.id>
2018-12-06 23:06:47 +0800

    Update coba.sh

--- coba.sh
+++ coba.sh
@@ -40,15 +40,15 @@ service ssh restart
 cat jcameron-key.asc | apt-key add -;rm jcameron-key.asc
 
 # set repo
-#cat > /etc/apt/sources.list <<END2
-#deb http://security.debian.org/ jessie/updates main contrib non-free
-#deb-src http://security.debian.org/ jessie/updates main contrib non-free
-#deb http://http.us.debian.org/debian jessie main contrib non-free
-#deb http://packages.dotdeb.org jessie all
-#deb-src http://packages.dotdeb.org jessie all
-#END2
-#wget "http://www.dotdeb.org/dotdeb.gpg"
-#cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+cat > /etc/apt/sources.list <<END2
+deb http://security.debian.org/ jessie/updates main contrib non-free
+deb-src http://security.debian.org/ jessie/updates main contrib non-free
+deb http://http.us.debian.org/debian jessie main contrib non-free
+deb http://packages.dotdeb.org jessie all
+deb-src http://packages.dotdeb.org jessie all
+END2
+wget "http://www.dotdeb.org/dotdeb.gpg"
+cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
 
 # remove unused
 apt-get -y --purge remove samba*;
