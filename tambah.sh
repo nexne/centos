1264c80a8408dec68ff4e117099b6ff766f498ff
nexne <nexne@unpak.ac.id>
2018-12-09 21:56:10 +0800

    Update tambah.sh

--- tambah.sh
+++ tambah.sh
@@ -6,14 +6,14 @@
 cd
 
 # disable ipv6
-echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
-sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+#echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+#sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
 
 #Add DNS Server ipv4
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
-echo "nameserver 8.8.4.4" >> /etc/resolv.conf
-sed -i '$ i\echo "nameserver 8.8.8.8" > /etc/resolv.conf' /etc/rc.local
-sed -i '$ i\echo "nameserver 8.8.4.4" >> /etc/resolv.conf' /etc/rc.local
+#echo "nameserver 8.8.8.8" > /etc/resolv.conf
+#echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+#sed -i '$ i\echo "nameserver 8.8.8.8" > /etc/resolv.conf' /etc/rc.local
+#sed -i '$ i\echo "nameserver 8.8.4.4" >> /etc/resolv.conf' /etc/rc.local
 
 # install wget and curl
 #apt-get update;apt-get -y install wget curl;
@@ -43,18 +43,18 @@ wget https://raw.githubusercontent.com/nexne/centos/master/install-premiumscript
 
 # Download script
 cd /usr/bin
-wget -O restart "https://raw.githubusercontent.com/nexne/32n64/master/resvis.sh"
-wget -O usernew "https://raw.githubusercontent.com/nexne/32n64/master/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/nexne/32n64/master/trial.sh"
-wget -O hapus "https://raw.githubusercontent.com/nexne/32n64/master/hapus.sh"
+#wget -O restart "https://raw.githubusercontent.com/nexne/32n64/master/resvis.sh"
+#wget -O usernew "https://raw.githubusercontent.com/nexne/32n64/master/usernew.sh"
+#wget -O trial "https://raw.githubusercontent.com/nexne/32n64/master/trial.sh"
+#wget -O hapus "https://raw.githubusercontent.com/nexne/32n64/master/hapus.sh"
 #wget -O login "https://raw.githubusercontent.com/nexne/32n64/master/user-login.sh"
-wget -O member "https://raw.githubusercontent.com/nexne/32n64/master/user-list.sh"
-wget -O speedtest "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/speedtest_cli.py"
-wget -O bench-network "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/bench-network.sh"
-wget -O ps-mem "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/ps_mem.py"
-wget -O about "https://raw.githubusercontent.com/nexne/32n64/master/about.sh"
+#wget -O member "https://raw.githubusercontent.com/nexne/32n64/master/user-list.sh"
+#wget -O speedtest "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/speedtest_cli.py"
+#wget -O bench-network "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/bench-network.sh"
+#wget -O ps-mem "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/ps_mem.py"
+#wget -O about "https://raw.githubusercontent.com/nexne/32n64/master/about.sh"
 wget -O delete "https://raw.githubusercontent.com/nexne/32n64/master/delete.sh"
-wget -O renew "https://raw.githubusercontent.com/nexne/32n64/master/renew.sh"
+#wget -O renew "https://raw.githubusercontent.com/nexne/32n64/master/renew.sh"
 wget -O kill "https://raw.githubusercontent.com/nexne/32n64/master/kill.sh"
 wget -O ban "https://raw.githubusercontent.com/nexne/32n64/master/ban.sh"
 wget -O unban "https://raw.githubusercontent.com/nexne/32n64/master/unban.sh"
@@ -66,20 +66,20 @@ echo "0 0 * * * root /root/user-expired.sh" > /etc/cron.d/user-expired
 echo "0 0 * * * root /usr/bin/reboot" > /etc/cron.d/reboot
 echo "#* * * * * service dropbear restart" > /etc/cron.d/dropbear
 #chmod +x menu
-chmod +x usernew
-chmod +x trial
+#chmod +x usernew
+#chmod +x trial
 chmod +x hapus
 #chmod +x login
 #chmod +x user-expired
 #chmod +x userlimit.sh
-chmod +x member
-chmod +x restart
-chmod +x speedtest
-chmod +x bench-network
-chmod +x ps-mem
-chmod +x about
+#chmod +x member
+#chmod +x restart
+#chmod +x speedtest
+#chmod +x bench-network
+#chmod +x ps-mem
+#chmod +x about
 chmod +x delete
-chmod +x renew
+#chmod +x renew
 #chmod +x user-expired.sh
 chmod +x kill
 chmod +x ban
